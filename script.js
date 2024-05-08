f74ba71ca5c3d1319053f050fd3a7b8cf40d4204
rizki a <rzkalqdr@gmail.com>
2024-05-08 16:10:26 +0700

    add: checkWinner() (Terrible Version)

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 const Gameboard = (() => {
 	const rows = 3;
 	const cols = 3;
-	const board = [
-		["", "", ""],
-		["", "", ""],
-		["", "", ""],
-	];
-
 	// const board = [
-	// 	["o", "x", "x"],
-	// 	["x", "o", "o"],
-	// 	["x", "o", "x"],
+	// 	["", "", ""],
+	// 	["", "", ""],
+	// 	["", "", ""],
 	// ];
 
+	// board: O winning
+	// const board = [
+	// 	["O", "X", "X"],
+	// 	["O", "O", "X"],
+	// 	["X", "O", "O"],
+	// ];
+
+	// board: X winning
+	// const board = [
+	// 	["X", "O", "O"],
+	// 	["X", "X", "O"],
+	// 	["O", "X", "X"],
+	// ];
+
+	// board: tie
+	const board = [
+		["X", "O", "X"],
+		["X", "O", "O"],
+		["O", "X", "X"],
+	];
+
+	const getRows = () => rows;
+	const getCols = () => cols;
+
 	const getBoard = () => board;
-	const getSpecificCell = (row, col) => board[row][col];
+	const getCell = (row, col) => board[row][col];
 	const setSpecificCell = (row, col, sign) => {
-		if (getSpecificCell(row, col) === "") {
+		if (getCell(row, col) === "") {
 			board[row][col] = sign;
 		} else {
 			// TODO: handle player switching when cell cannot be played
@@ -29,8 +47,8 @@ const Gameboard = (() => {
 		for (let row = 0; row < rows; row++) {
 			let currentRow = "";
 			for (let col = 0; col < cols; col++) {
-				if (getSpecificCell(row, col) !== "") {
-					currentRow = currentRow + " " + getSpecificCell(row, col);
+				if (getCell(row, col) !== "") {
+					currentRow = currentRow + " " + getCell(row, col);
 				} else {
 					currentRow = currentRow + " " + "_";
 				}
@@ -40,12 +58,22 @@ const Gameboard = (() => {
 		console.log(boardString);
 	};
 
-	return { getBoard, getSpecificCell, setSpecificCell, printBoard };
+	return {
+		getRows,
+		getCols,
+		getBoard,
+		getCell,
+		setSpecificCell,
+		printBoard,
+	};
 })();
 
 // TODO: game controller; controlling the game state and flow
 function GameController() {
 	const board = Gameboard;
+	// for checkWinner
+	const rows = Gameboard.getRows();
+	const cols = Gameboard.getCols();
 
 	const Player = (name, sign) => {
 		const playerName = name;
@@ -84,8 +112,53 @@ function GameController() {
 		switchCurrentPlayer();
 	};
 
+	// TODO: determine game over situation(s) (one player wins, tie)
+	// this is terribleeeeeee and shouldnt be hardcoded but whatever (maybe ill fix it later)
+	const checkWinner = () => {
+		// check for horizontal winner
+		for (let row = 0; row < rows; row++) {
+			if (
+				board.getCell(row, 0) !== "" &&
+				board.getCell(row, 0) === board.getCell(row, 1) &&
+				board.getCell(row, 0) === board.getCell(row, 2)
+			) {
+				return board.getCell(row, 0);
+			}
+		}
+
+		// check for vertical winner
+		for (let col = 0; col < cols; col++) {
+			if (
+				board.getCell(0, col) !== "" &&
+				board.getCell(0, col) === board.getCell(1, col) &&
+				board.getCell(0, col) === board.getCell(2, col)
+			) {
+				return board.getCell(0, col);
+			}
+		}
+
+		// check for diagonal winner
+		if (
+			board.getCell(0, 0) !== "" &&
+			board.getCell(0, 0) === board.getCell(1, 1) &&
+			board.getCell(0, 0) === board.getCell(2, 2)
+		) {
+			return board.getCell(0, 0);
+		}
+
+		if (
+			board.getCell(0, 2) !== "" &&
+			board.getCell(0, 2) === board.getCell(1, 1) &&
+			board.getCell(0, 2) === board.getCell(2, 0)
+		) {
+			return board.getCell(0, 2);
+		}
+
+		return null;
+	};
+
 	// TODO: remove "board" later when done debugging
-	return { getCurrentPlayer, switchCurrentPlayer, setMove, board };
+	return { getCurrentPlayer, switchCurrentPlayer, setMove, checkWinner, board };
 }
 
 const game = GameController();
